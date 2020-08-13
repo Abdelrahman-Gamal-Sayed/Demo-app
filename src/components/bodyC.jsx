13be7c30edb9e343ec25e587a9bd86b674e4c40a
Abdelrahman-Gamal-Sayed <Abdelrahman.Gamal.Sayed@gmail.com>
2020-08-13 12:57:57 +0200

    hhhlast

--- src/components/bodyC.jsx
+++ src/components/bodyC.jsx
@@ -3,105 +3,176 @@ import axios from 'axios'
 
 
 class BodyC extends Component {
-  state = {   
-    dataa:[],
-    bitrhdate:''
-   }
 
 
-   datestring()
+ 
+    state = {   
+      dataa:[],
+      PatientInsurance:[],
+      Contacts:[]
+     }
+    
+
+
+
+   datestring(d)
    {
-
-  
-   var str = this.state.dataa.DOB;
-var ar = str.split(',');
-return ar[0]+'/'+ar[1];
+    if(d===undefined)
+      return"";
+    
+    var ar = d.split(',');
+    return ar[0]+'/'+ar[1]+'/'+ar[2];
    }
+
+
 
  async componentDidMount() {
       const {data:dataa}= await axios.get('http://3.120.165.103:203/api/patient/getpatient/205609/853');
 
-      var str = dataa.DOB;
-      var ar = str.split(',');
-const bitrhdate=ar[0]+'/'+ar[1]+'/'+ar[2];
-this.setState({dataa,bitrhdate});
-   console.log(dataa.PatientContacts);
+
+console.log('aaa',dataa.PatientContacts[0].ContactType)
+const PatientInsurance=dataa.PatientInsuranceCards;
+const Contacts=dataa.PatientContacts[0].ContactType;
+this.setState({dataa,PatientInsurance,Contacts});
+   
     }
+
+
+
   render() { 
       return ( 
       
 
 
 
-    
-
-
-
-
-        <div className='container-fluid'  style={{ marginTop:70 }}>
-
-
-
-          
+        <div class='container-fluid'  style={{ marginTop:70 }}>
 
 <div className='row'>
+
   <div className='col-md-3'>
-
-
 
 
   <div class="card" >
   <img src="http://via.placeholder.com/250" class="img-rounded"/>
-  <div class="card-body">
-    <h5 class="card-title">{this.state.dataa.FirstName +' '+this.state.dataa.LastName}</h5>
-    <p class="card-text">ID : {this.state.dataa.PatientID}</p>
+  <div class="card-body"  className='text-center'>
+  <h5  class="card-title">{this.state.dataa.FirstName +' '+this.state.dataa.LastName}</h5>
   </div>
+
+  <div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">{this.state.bitrhdate}</li>
-    <li class="list-group-item">{this.state.dataa.FileNumber}</li>
-  
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Edite</a>
-   
-  </div>
-</div>
+  <li class="list-group-item">
 
-
-    <div className='text-center'>
-
-    <svg style={{width:24,height:24,display:"inline-blockck"}}   viewBox="0 0 24 24">
-    <path fill="currentColor" d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+  <div className='blockk'>
+<svg style={{marginRight:15}}  width="1em" height="2em" viewBox="0 0 16 16" class="bi bi-credit-card-2-front" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+  <path d="M2 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z"/>
+  <path fill-rule="evenodd" d="M2 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
 </svg>
+    {this.state.dataa.PatientID}
+</div>
 
-
-
-
-
-    </div>
+  </li>
+<li class="list-group-item">
   
+
+
+<svg style={{marginRight:15}} width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar4-week" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M14 2H2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM2 1a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"/>
+<path fill-rule="evenodd" d="M14 2H2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zM2 1a2 2 0 0 0-2 2v2h16V3a2 2 0 0 0-2-2H2z"/>
+<path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"/>
+<path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+</svg>
+  { this.datestring(this.state.dataa.DOB)  }
+  
+  </li>
+
+
+
+
+<li class="list-group-item">
+<svg style={{marginRight:15}} width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-files" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M3 2h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3z"/>
+<path d="M5 0h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1H3a2 2 0 0 1 2-2z"/>
+</svg>
+  
+  {this.state.dataa.FileNumber}</li>
+<li class="list-group-item"> <div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="defaultCheck2" disabled />
+<label class="form-check-label" for="defaultCheck2">
+GuaranteeOfPayment
+</label>
+</div></li>
+
+
+
+</ul>
+<div class="card-body">
+<a href="#" class="card-link">Edite</a>
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
   </div>
 
-  <div className='col-md-8'>
-    
+  <div class='col-md-8'>
+  
+
+
   <div class="card bg-light mb-3" >
-  <div class="card-header">Header</div>
+  <div class="card-header">ContactType</div>
   <div class="card-body">
-    <h5 class="card-title">Light card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+      <p class="card-text">ContactTypeDesc : {this.state.Contacts.ContactTypeDesc}</p>
+      <p class="card-text">ContactTypeID : {this.state.Contacts.ContactTypeID}</p>
+      <p class="card-text">CreatedOn : {this.datestring(this.state.Contacts.CreatedOn)}</p>
+      <p class="card-text">ModifiedOn : {this.datestring(this.state.Contacts.ModifiedOn)}</p>
+
   </div>
 </div>
 
 
-    <p>
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+  <div class="card bg-light mb-3" >
+  <div class="card-header">PatientInsuranceCards</div>
+  <div class="card-body">
+  
+      <p class="card-text"> {this.state.PatientInsurance.map(m=><p>CardID : {m.PatientInsuranceCardID} <br/> InsuranceCompanyID : {m.InsuranceCompanyID} <br/>PatientCardNumber : {m.PatientCardNumber} <br/> CreatedOn : {this.datestring(m.CreatedOn)}<hr/></p>)}</p>
+  </div>
+</div>
 
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
 
-Where can I get some?
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
-    </div>
+
+
+
+
+  <div class="card bg-light mb-3" >
+  <div class="card-header">SocialSecurityNumber</div>
+  <div class="card-body">
+    <h5 class="card-title">{this.state.dataa.SocialSecurityNumber}</h5>
+ </div>
+</div>
+
+
+
+<div class="card bg-light mb-3" >
+  <div class="card-header">FileNumber</div>
+  <div class="card-body">
+    <h5 class="card-title">{this.state.dataa.FileNumber}</h5>
+  </div>
+</div>
+
+
+
+  </div>
     
     
 </div>
